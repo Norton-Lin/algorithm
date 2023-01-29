c5e002edb5c90d05adcfa578b71962631c528bd8
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-01-30 00:32:25 +0800

    LeetCode: 1669

---
+++ java/src/leetcode_1669/Solution.java
@@ -0,0 +1,32 @@
+package leetcode_1669;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode tail = new ListNode();
+        ListNode cur = list1;
+        int count = 0;
+        for(int i =0;i<b;++i,++count,cur = cur.next)
+        {
+        	if(count == a-1)
+        		head = cur;
+        }
+        tail = cur.next;
+        head.next = list2;
+        cur = list2;
+        while(cur.next!=null)
+        	cur = cur.next;
+        cur.next = tail;
+        return list1;
+        
+    }
+}
