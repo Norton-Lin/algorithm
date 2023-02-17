5317ee9a3106f5e4e2a9033f5e636c165de14a60
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-02-17 21:49:42 +0800

    LeetCode: 1139,2341

---
+++ java/src/leetcode_2341/Solution.java
@@ -0,0 +1,21 @@
+package leetcode_2341;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] numberOfPairs(int[] nums) {
+        int ans = 0;
+        Arrays.sort(nums);
+        for(int i =0;i<nums.length-1;)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                ++ans;
+                i+=2;
+            }
+            else
+                ++i;
+        }
+        return new int[]{ans,nums.length-2*ans};
+    }
+}
