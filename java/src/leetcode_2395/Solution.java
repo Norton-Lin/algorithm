e47a4d71f3fe48ac588a284170922a66d9d3e836
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-03-26 10:23:44 +0800

    LeetCode:2395

---
+++ java/src/leetcode_2395/Solution.java
@@ -0,0 +1,36 @@
+package leetcode_2395;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+	//1 相加搜索 O(n^2)
+	//2 相加后排序 O(nlogn)
+	//3 哈希 O(n)
+	/**
+    public boolean findSubarrays(int[] nums) {
+        for(int i = 0;i<nums.length-1;++i)
+            nums[i] = nums[i] + nums[i+1];
+        for(int i = 0;i<nums.length-1;++i)
+        {
+            for(int j = i+1;j<nums.length-1;++j)
+                if(nums[i] == nums[j])
+                    return true;
+        }
+        return false;
+    }*/
+	public boolean findSubarrays(int[] nums) {
+		HashSet<Integer> set  = new HashSet<>(null);
+		int num = 0;
+		for(int i = 1;i<nums.length;++i)
+		{
+			num = nums[i]+ nums[i-1];
+			if(set.contains(num))
+				return true;
+			else 
+				set.add(num);
+		}
+		return false;
+	}
+
+}
