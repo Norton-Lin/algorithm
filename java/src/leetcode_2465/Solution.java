78565740f008358f264745b53457058bb4c77116
Norton-Lin <75243447+Norton-Lin@users.noreply.github.com>
2023-06-04 12:37:00 +0800

    LeetCode: 2465

---
+++ java/src/leetcode_2465/Solution.java
@@ -0,0 +1,17 @@
+package leetcode_2465;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Solution {
+	//排序双指针+哈希
+    public int distinctAverages(int[] nums) {
+    	HashSet<Integer> set = new HashSet<>();
+    	Arrays.sort(nums);
+    	int i = 0,j = nums.length-1;
+    	while(i<j)
+    		set.add(nums[i++]+nums[j--]);
+    	return set.size();
+    	
+    }
+}
