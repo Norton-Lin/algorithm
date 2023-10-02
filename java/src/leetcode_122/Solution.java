4a7cdf068ef32d2ba9467550ef58f489472acd40
Norton-Lin <358154247@qq.com>
2023-10-02 09:53:49 +0800

    LeetCode: 122

---
+++ java/src/leetcode_122/Solution.java
@@ -0,0 +1,16 @@
+package leetcode_122;
+
+class Solution {
+    //买卖多支股票，同时最多持有一只
+    //贪心，高抛低吸
+    //可以在同一天买卖同一支股票
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int cur = 0;
+        for(int i = 0;i<prices.length;++i)
+        {
+            if()
+        }
+        return res;
+    }
+}
