ba4cbcb5c7a452894fe22852d4baa588b983476c
Norton-Lin <358154247@qq.com>
2023-11-20 12:41:36 +0800

    LeetCode: 53

---
+++ java/src/leetcode_53/Solution.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ans = Integer.MIN_VALUE;
+        int cur = 0;
+        for(int i:nums){
+            cur+=i;
+            if(cur<i)
+                cur = i;
+            ans = Math.max(cur,ans);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
