20a586e51db35413abdf32438acf4bb267ae6b1e
Norton-Lin <358154247@qq.com>
2023-12-03 13:53:49 +0800

    LeetCode: 1423、2661

--- java/src/leetcode_2661/Solution.java
+++ java/src/leetcode_2661/Solution.java
@@ -9,7 +9,24 @@ class Solution {
     //某一行或列完全上色时，返回当前arr下标
     //邻接表？
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        int
+        int[][] record = new int[arr.length+1][2];
+        int[] row = new int[mat.length];
+        int[] col = new int[mat[0].length];
+        for(int i = 0;i<mat.length;++i){
+            for(int j = 0;j<mat[i].length;++j){
+                record[mat[i][j]][0] = i;
+                record[mat[i][j]][1] = j;
+            }
+        }
+        int count = 0;
+        while(count<arr.length){
+            ++row[record[arr[count]][0]];
+            ++col[record[arr[count]][1]];
+            if(row[record[arr[count]][0]] == mat[0].length||col[record[arr[count]][1]]==mat.length)
+                break;
+            ++count;
+        }
+        return count;
     }
 }
 // @lc code=end
