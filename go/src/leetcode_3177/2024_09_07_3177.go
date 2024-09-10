bb86e81c544a0840af8cbc3cc1363e71e09639b2
Norton-Lin's Mac Book Air <358154247@qq.com>
2024-09-10 14:27:01 +0800

    LeetCode(day):3177

--- go/src/leetcode_3177/2024_09_07_3177.go
+++ go/src/leetcode_3177/2024_09_07_3177.go
@@ -1,3 +1,5 @@
+package main
+
 /*
  * @lc app=leetcode.cn id=3177 lang=golang
  *
@@ -5,8 +7,20 @@
  */
 
 // @lc code=start
-func maximumLength(nums []int, k int) int {
-
+func MaximumLength(nums []int, k int) int {
+	record := map[int][]int{}
+	ans := make([]int, k+2)
+	for _, i := range nums {
+		if record[i] == nil {
+			record[i] = make([]int, k+1)
+		}
+		cur := record[i]
+		for j := k; j >= 0; j-- {
+			cur[j] = max(cur[j], ans[j]) + 1
+			ans[j+1] = max(ans[j+1], cur[j])
+		}
+	}
+	return ans[k+1]
 }
-// @lc code=end
 
+// @lc code=end
