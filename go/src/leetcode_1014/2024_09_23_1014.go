ad1617cba543c59809b29eddc4a842d6d1af061b
Norton-Lin's Mac Book Air <358154247@qq.com>
2024-09-23 19:50:54 +0800

    LeetCode(day): 1014

---
+++ go/src/leetcode_1014/2024_09_23_1014.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=1014 lang=golang
+ *
+ * [1014] 最佳观光组合
+ */
+
+// @lc code=start
+func maxScoreSightseeingPair(values []int) int {
+	ans := values[0] + values[1] - 1
+	cur := ans
+	n := len(values)
+	for i := 2; i < n; i++ {
+		cur = max(cur+values[i]-values[i-1]-1, values[i]+values[i-1]-1)
+		ans = max(cur, ans)
+
+	}
+	return ans
+}
+
+// @lc code=end
