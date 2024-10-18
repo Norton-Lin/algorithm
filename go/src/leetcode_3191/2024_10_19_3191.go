633c1c539377ca3aefa5cd818941e67c1068d318
Norton-Lin's Mac Book Air <358154247@qq.com>
2024-10-18 15:34:14 +0800

    LeetCode(day): 3191

---
+++ go/src/leetcode_3191/2024_10_19_3191.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=3191 lang=golang
+ *
+ * [3191] 使二进制数组全部等于 1 的最少操作次数 I
+ */
+
+// @lc code=start
+func minOperations(nums []int) int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+
+	}
+}
+
+// @lc code=end
