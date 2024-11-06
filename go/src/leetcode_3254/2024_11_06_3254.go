f5b0ee5738cccd91d4319bbcb7efb14d271c6820
Norton-Lin's Mac Book Air <358154247@qq.com>
2024-11-06 15:18:42 +0800

    LeetCode(day): 3254

---
+++ go/src/leetcode_3254/2024_11_06_3254.go
@@ -0,0 +1,34 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=3254 lang=golang
+ *
+ * [3254] 长度为 K 的子数组的能量值 I
+ */
+
+// @lc code=start
+// 一个中断接下来k个中断
+// 上一个连续，新数字也连续，新数组连续
+// if dp[i-1] != -1 && nums[i+k] == nums[i-1]+k+1
+// dp[i] = do[i-1]+1
+// else dp[i] = -1
+func resultsArray(nums []int, k int) []int {
+	n := len(nums)
+	ans := make([]int, n-k+1)
+	i := 1
+	for ;i<k;i++{
+		if nums[i]!=nums[i-1]+1{
+			ans[0] = -1
+			break;
+		}
+	}
+	if ans[0] != -1 {
+		ans[0] = nums[i-1]
+	}
+	for i < n ;i++{
+		
+	}
+	return ans
+}
+
+// @lc code=end
