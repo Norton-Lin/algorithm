c7f9d6a4dee3e0e225b304081d5878621ad6049f
Norton-Lin's Desktop <358154247@qq.com>
2024-11-23 21:35:28 +0800

    feat(day): 3238

---
+++ go/src/leetcode_3238/2024_11_23_3238.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=3238 lang=golang
+ *
+ * [3238] 求出胜利玩家的数目
+ */
+
+// @lc code=start
+func winningPlayerCount(n int, pick [][]int) int {
+	records := make([][]int, n)
+	ans := 0
+	for i := 0; i < n; i++ {
+		records[i] = make([]int, 11)
+	}
+	for _, nums := range pick {
+		records[nums[0]][nums[1]]++
+	}
+	for i, record := range records {
+		if record > i {
+			ans++
+		}
+	}
+	return ans
+}
+
+// @lc code=end
