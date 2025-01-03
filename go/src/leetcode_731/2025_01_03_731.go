849b409b7fe92a5b7be55a7f4fefad651e78b0d6
Norton-Lin's Desktop <358154247@qq.com>
2025-01-03 21:55:05 +0800

    LeetCode(day): 731

---
+++ go/src/leetcode_731/2025_01_03_731.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=731 lang=golang
+ *
+ * [731] 我的日程安排表 II
+ */
+
+// @lc code=start
+type pair struct{ start, end int }
+type MyCalendarTwo struct{ booked, overlaps []pair }
+
+func Constructor() MyCalendarTwo {
+	return MyCalendarTwo{}
+}
+
+func (c *MyCalendarTwo) Book(start, end int) bool {
+	for _, p := range c.overlaps {
+		if p.start < end && start < p.end {
+			return false
+		}
+	}
+	for _, p := range c.booked {
+		if p.start < end && start < p.end {
+			c.overlaps = append(c.overlaps, pair{max(p.start, start), min(p.end, end)})
+		}
+	}
+	c.booked = append(c.booked, pair{start, end})
+	return true
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Book(startTime,endTime);
+ */
+// @lc code=end
