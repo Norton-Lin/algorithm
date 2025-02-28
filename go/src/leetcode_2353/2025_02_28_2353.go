4c6e02db266210078fe09de1ac24da2423ce3314
Norton-Lin <358154247@qq.com>
2025-02-28 10:08:47 +0800

    LeetCode(day):2353

---
+++ go/src/leetcode_2353/2025_02_28_2353.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"container/heap"
+)
+
+/*
+ * @lc app=leetcode.cn id=2353 lang=golang
+ *
+ * [2353] 设计食物评分系统
+ */
+
+// @lc code=start
+type FoodRatings struct {
+	foodToCuisine map[string]string
+	foodToRating  map[string]int
+	cuisineToFood map[string]*MaxHeap
+}
+
+func Constructor(foods []string, cuisines []string, ratings []int) FoodRatings {
+	foodToCuisine := make(map[string]string)
+	foodToRating := make(map[string]int)
+	cuisineToFood := make(map[string]*MaxHeap)
+
+	for i, food := range foods {
+		foodToCuisine[food] = cuisines[i]
+		foodToRating[food] = ratings[i]
+		if _, ok := cuisineToFood[cuisines[i]]; !ok {
+			cuisineToFood[cuisines[i]] = &MaxHeap{}
+			heap.Init(cuisineToFood[cuisines[i]])
+		}
+		heap.Push(cuisineToFood[cuisines[i]], &Food{food, ratings[i]})
+	}
+
+	return FoodRatings{foodToCuisine, foodToRating, cuisineToFood}
+}
+
+func (this *FoodRatings) ChangeRating(food string, newRating int) {
+	cuisine := this.foodToCuisine[food]
+	this.foodToRating[food] = newRating
+
+	// 更新堆中的评分
+	heap.Push(this.cuisineToFood[cuisine], &Food{food, newRating})
+}
+
+func (this *FoodRatings) HighestRated(cuisine string) string {
+	maxHeap := this.cuisineToFood[cuisine]
+	for {
+		top := heap.Pop(maxHeap).(*Food)
+		if this.foodToRating[top.name] == top.rating {
+			heap.Push(maxHeap, top)
+			return top.name
+		}
+	}
+}
+
+type Food struct {
+	name   string
+	rating int
+}
+
+type MaxHeap []*Food
+
+func (h MaxHeap) Len() int { return len(h) }
+func (h MaxHeap) Less(i, j int) bool {
+	return h[i].rating > h[j].rating || (h[i].rating == h[j].rating && h[i].name < h[j].name)
+}
+func (h MaxHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(*Food))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	item := old[n-1]
+	*h = old[0 : n-1]
+	return item
+}
+
+// @lc code=end
