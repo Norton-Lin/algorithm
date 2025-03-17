f614a30a8e0d097926faab342a967495ec135d89
Norton-Lin's Desktop <358154247@qq.com>
2025-03-17 10:40:31 +0800

    LeetCode(contest): 441

---
+++ go/src/leetcode_2272/2025_03_17_2272.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=2272 lang=golang
+ *
+ * [2272] 最大波动的子字符串
+ */
+
+// @lc code=start
+func largestVariance(s string) int {
+	return 0
+}
+
+// @lc code=end
