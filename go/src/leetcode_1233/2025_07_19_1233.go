c6f6a6e83fc35b291fc6965d847e384996cf360d
Norton_Lin's Desktop <358154247@qq.com>
2025-07-19 21:49:07 +0800

    LeetCode(day): 1233

---
+++ go/src/leetcode_1233/2025_07_19_1233.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"sort"
+	"strings"
+)
+
+/*
+ * @lc app=leetcode.cn id=1233 lang=golang
+ *
+ * [1233] 删除子文件夹
+ */
+
+// @lc code=start
+func removeSubfolders(folder []string) []string {
+	sort.Strings(folder)
+	ans := make([]string, 0)
+	cur := folder[0]
+	ans = append(ans, cur)
+	for _, str := range folder[1:] {
+		if strings.HasPrefix(str, cur+"/") {
+			continue
+		} else {
+			ans = append(ans, str)
+			cur = str
+		}
+	}
+	return ans
+}
+
+// @lc code=end
